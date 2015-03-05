b58c11e2dbcd45d07da90c2e31c0d452ff756484
Dave Carruthers <dave@dweb-x.com>
2015-03-05 03:30:25 +0000

    Added key check and validation to key activity

--- app/src/main/java/com/dweb_x/movielist/KeyActivity.java
+++ app/src/main/java/com/dweb_x/movielist/KeyActivity.java
@@ -3,8 +3,11 @@ package com.dweb_x.movielist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -16,13 +19,37 @@ import android.widget.TextView;
 public class KeyActivity extends ActionBarActivity {
 
     private TextView keyView;
+    private MovieList list = MovieList.getInstance(this);
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addkey);
         keyView = (TextView)findViewById(R.id.keyCheckText);
+        keyView.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                Log.v("Key text changed",s.toString());
+                validText(s.toString());
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
     }
+
+    /*
+     *  Enables the button if passed in string is in size range and unique.
+     */
+    private void validText(String s){
+        if(s.length() > 0 && list.isKeyAvailable(s)){
+            btn = (Button)findViewById(R.id.keyCheckButton);
+            btn.setEnabled(true);
+        } else {
+            btn.setEnabled(false);
+        }
+
+    }
+
 // add method to check key when text view drops focus ------------------------ !!! todo
     /**
      * onClick handler for keyCheck button.
